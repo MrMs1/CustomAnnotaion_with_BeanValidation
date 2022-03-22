11f209af16fe7a9e4ebde362720b25472660d98d
MrMs1 <46705152+MrMs1@users.noreply.github.com>
2022-03-23 01:12:06 +0900

    テストクラスを修正
    共通処理の集約
    テストケース文言の修正

--- src/test/java/customannotation/AnnotationDemoTest.java
+++ src/test/java/customannotation/AnnotationDemoTest.java
@@ -14,62 +14,51 @@ import jakarta.validation.ValidatorFactory;
 
 class AnnotationDemoTest {
 
-	@Test
-	@DisplayName("dateが2022/01/01 11:11:11のとき、バリエーション結果のメッセージはnull")
-	void dateが存在する日付のときバリエーション結果のメッセージはnull() {
-		// 準備
-		AnnotationDemo target = new AnnotationDemo("2022/01/01 11:11:11");
+	private static final String DATE = "date";
+	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	private Validator validator = factory.getValidator();
 
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
-		// 実行
-		Set<ConstraintViolation<AnnotationDemo>> constraintViolations = validator.validate(target);
-		// 検証
-		assertEquals(0, constraintViolations.size());
+	private String getValidateResultMessage(String fieldName, String value) {
+		return validator
+				.validateValue(AnnotationDemo.class, fieldName, value)
+				.iterator()
+				.next()
+				.getMessage();
+	}
+
+	private boolean isValidateResult(String fieldName, String value) {
+		return validator
+				.validateValue(AnnotationDemo.class, fieldName, value)
+				.iterator()
+				.hasNext();
 	}
 
 	@Test
-	@DisplayName("dateがnullのとき、バリエーション結果のメッセージはnull")
-	void dateがnullのときバリエーション結果のメッセージはnull() {
-		// 準備
-		AnnotationDemo target = new AnnotationDemo(null);
-
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
-		// 実行
-		Set<ConstraintViolation<AnnotationDemo>> constraintViolations = validator.validate(target);
+	@DisplayName("dateが2022/01/01 11:11:11のとき、バリエーション結果は存在しない")
+	void dateが存在する日付のときバリエーション結果は存在しない() {
 		// 検証
-		assertEquals(0, constraintViolations.size());
+		assertEquals(false, isValidateResult(DATE, "2022/01/01 11:11:11"));
+	}
+
+	@Test
+	@DisplayName("dateがnullのとき、バリエーション結果は存在しない")
+	void dateがnullのときバリエーション結果は存在しない() {
+		// 検証
+		assertEquals(false, isValidateResult(DATE, null));
 	}
 
 	@Test
 	@DisplayName("dateが2022/01/01のとき、バリエーション結果のメッセージは「存在する日付を指定してください。」である")
 	void dateが年月日のみのときバリエーション結果のメッセージが存在する() {
-		// 準備
-		AnnotationDemo target = new AnnotationDemo("2022/01/01");
-
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
-		// 実行
-		Set<ConstraintViolation<AnnotationDemo>> constraintViolations = validator.validate(target);
 		// 検証
-		assertEquals(1, constraintViolations.size());
-		assertEquals("yyyy/MM/dd HH:mm:ss形式で設定してください。", constraintViolations.iterator().next().getMessage());
+		assertEquals("yyyy/MM/dd HH:mm:ss形式で設定してください。", getValidateResultMessage(DATE, "2022/01/01"));
 	}
 
 	@Test
 	@DisplayName("dateが2022/02/31 11:11:11のとき、バリエーション結果のメッセージは「存在する日付を指定してください。」である")
 	void dateに存在しない秒が含まれているときバリエーション結果のメッセージが存在する() {
-		// 準備
-		AnnotationDemo target = new AnnotationDemo("2022/02/31 11:11:11");
-
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
-		// 実行
-		Set<ConstraintViolation<AnnotationDemo>> constraintViolations = validator.validate(target);
 		// 検証
-		assertEquals(1, constraintViolations.size());
-		assertEquals("存在する日付を指定してください。", constraintViolations.iterator().next().getMessage());
+		assertEquals("存在する日付を指定してください。", getValidateResultMessage(DATE, "2022/02/31 11:11:11"));
 	}
 
 }
